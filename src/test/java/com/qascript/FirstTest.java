9379c6107710f98c40c12a6fb25b2c07f8eb8af8
raknas999 <32408560+raknas999@users.noreply.github.com>
2023-01-08 14:28:44 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -13,10 +13,15 @@ public class FirstTest {
     public void OpenBrowser()  {
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.out.println("Title of the page is-1");
         ChromeOptions options = new ChromeOptions();
+                System.out.println("Title of the page is-2");
         options.addArguments("headless");
+                System.out.println("Title of the page is-3");
         options.addArguments("disable-gpu");
+                System.out.println("Title of the page is-4");
         driver = new ChromeDriver(options);
+                System.out.println("Title of the page is-5");
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
